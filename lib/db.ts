e4af78ea9caa43143f1f18c5840101cb4fa20b86
MaartenDeKlerkPXL <maarten.deklerk@student.pxl.be>
2026-02-01 17:58:46 +0100

    Final TS fix and db export

--- lib/db.ts
+++ lib/db.ts
@@ -2,7 +2,11 @@ import { PrismaClient } from '@prisma/client'
 
 const globalForPrisma = global as unknown as { prisma: PrismaClient }
 
-// HIER MOET 'db' STAAN
-export const db = globalForPrisma.prisma || new PrismaClient()
+// We maken de client aan
+const client = globalForPrisma.prisma || new PrismaClient()
 
-if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = db
+if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = client
+
+// We exporteren hem onder BEIDE namen zodat elk bestand blij is
+export { client as db }
+export { client as prisma }
